8a025531565e9708ed4a9f1b22b6b8062eedd6b3
jn2152 <joel.negrillo@adventhealth.com>
2024-05-28 15:20:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-st.text(fruityvice_response)
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -28,6 +26,9 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+st.text(fruityvice_response)
 
 if ingredients_list:
     ingredients_string = ''
